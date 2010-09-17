5f50083d6f09de44e1f157db19d19af11047cbe7
Wale Hassan <wale@wale-laptop.(none)>
2010-09-16 23:12:27 -0500

    tied online menu page to online versus moveset select

--- onlinemenupage.py
+++ onlinemenupage.py
@@ -7,6 +7,7 @@ import button
 
 host_match_button = None
 join_match_button = None
+menu = None
 
 exit_button = None
 exit_indicator = False
@@ -16,28 +17,56 @@ def load():
     global exit_button
     global exit_indicator
     global loaded
+    global menu
+    global host_match_button
+    global join_match_button
     
     exit_button = button.ExitButton()
     exit_indicator = False
     loaded = True
     
+    #Create menu buttons
+    host_match_button = \
+        menupage.MenuButton(
+            "Host Match", 
+            gamestate.Modes.ONLINEVERSUSMOVESETSELECT
+        )
+    
+    join_match_button = \
+        menupage.MenuButton(
+            "Join Match",
+            gamestate.Modes.ONLINEVERSUSMOVESETSELECT
+        )
+    
+    #create module menu object
+    menu = menupage.Menu()
+    
+    menu.load([host_match_button, join_match_button])
+    
 def unload():
     global exit_button
     global exit_indicator
     global loaded
+    global menu
+    global host_match_button
+    global join_match_button
     
     exit_button = None
     exit_indicator = False
     loaded = False
+    host_match_button = None
+    join_match_button = None
+    menu = None
 
 def handle_events():
     global exit_button
     global exit_indicator
+    global menu
     
     if not loaded:
         load()
     
-    exit_button.draw(gamestate.screen)
+    menu.handle_events()
     
     if pygame.MOUSEBUTTONDOWN in wotsuievents.event_types:
         if exit_button.contains(wotsuievents.mouse_pos):
@@ -53,3 +82,7 @@ def handle_events():
             if exit_button.contains(wotsuievents.mouse_pos):
                 unload()
                 gamestate.mode = gamestate.Modes.MAINMENU
+    
+    if loaded:
+        exit_button.draw(gamestate.screen)
+        menu.draw(gamestate.screen)
