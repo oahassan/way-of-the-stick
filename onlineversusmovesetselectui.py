eb2472eb7dbd408ccdb73600596268c6d59a425a
Wale Hassan <wale@wale-laptop.(none)>
2010-09-19 15:57:09 -0500

    created LocalPlayerSetupContainer UI class

--- onlineversusmovesetselectui.py
+++ onlineversusmovesetselectui.py
@@ -18,3 +18,45 @@ class NetworkMessageNotification(button.Label):
     
     def expired(self):
         return self.timer > self.timeout
+
+class LocalPlayerSetupContainer(wotsui.UIObjectBase):
+    
+    def __init__(self, position, movesets):
+        
+        player_type_select_position = position
+        
+        self.player_type_select = \
+            wotsuicontainers.ButtonContainer(
+                player_type_select_position,
+                200,
+                300,
+                'Select Player Type',
+                button.TextButton,
+                [['Human',15], ['Bot',15]]
+            )
+        
+        moveset_select_position = \
+            (
+                player_type_select_position[0],
+                player_type_select_position[1] + self.player_type_select.height + 20
+            )
+        self.moveset_select = \
+            movesetselectui.MovesetSelectContainer(
+                moveset_select_position,
+                200, \
+                100, \
+                'Select Your Moveset', \
+                movesets
+            )
+    
+    def handle_events():
+    
+        player_moveset_select.handle_events()
+        bot_moveset_select.handle_events()
+    
+    def player_ready():
+        if ((player_moveset_select.selected_moveset != None) and
+            (bot_moveset_select.selected_moveset != None)):
+            return True
+        else:
+            return False
