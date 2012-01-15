9711d25fcebd4e2289cac3334db3fa9a6baf6ebc
Wale Hassan <oahassan@gmail.com>
2012-01-15 11:06:59 -0600

    created container object for versus moveset select UI state

--- versusmovesetselect.py
+++ versusmovesetselect.py
@@ -11,50 +11,30 @@ import wotsuicontainers
 from versusmovesetselectui import MovesetSelector, PlayerStatsWidget, DifficultyLabel, ColorWheel
 from enumerations import PlayerPositions, PlayerTypes, PlayerStates, Difficulties
 
-loaded = False
-exit_button = None
-start_match_label = None
-player1_type_select = None
-player1_difficulty_select = None
-player1_moveset_select = None
-player1_stats_widget = None
-player1_color_select = None
-player2_type_select = None
-player2_difficulty_select = None
-player2_moveset_select = None
-player2_stats_widget = None
-player2_color_select = None
-
-def get_playable_movesets():
-    movesets = movesetdata.get_movesets()
-    playable_movesets = [moveset for moveset in movesets if moveset.is_complete()]
-    
-    return playable_movesets
-
-def load():
-    global loaded
-    global exit_button
-    global start_match_label
-    global player1_type_select
-    global player1_difficulty_select
-    global player1_moveset_select
-    global player1_stats_widget
-    global player1_color_select
-    global player2_type_select
-    global player2_difficulty_select
-    global player2_moveset_select
-    global player2_stats_widget
-    global player2_color_select
-    
-    exit_button = button.ExitButton()
-    loaded = True
-    start_match_label = movesetselectui.MovesetActionLabel((50, 550), "Start Match!")
-    start_match_label.inactivate()
-    
-    if player1_moveset_select == None:
+class VersusMovesetSelectUiObjects():
+    def __init__(self):        
+        
+        self.start_match_label = None
+        self.player1_type_select = None
+        self.player1_difficulty_select = None
+        self.player1_moveset_select = None
+        self.player1_stats_widget = None
+        self.player1_color_select = None
+        self.player2_type_select = None
+        self.player2_difficulty_select = None
+        self.player2_moveset_select = None
+        self.player2_stats_widget = None
+        self.player2_color_select = None
+        
+        self.init_UI_objects()
+        
+    def init_UI_objects(self):
+        self.start_match_label = movesetselectui.MovesetActionLabel((50, 550), "Start Match!")
+        self.start_match_label.inactivate()
+        
         playable_movesets = get_playable_movesets()
         
-        player1_type_select = wotsuicontainers.ButtonContainer(
+        self.player1_type_select = wotsuicontainers.ButtonContainer(
             (50,50),
             120,
             300,
@@ -62,12 +42,12 @@ def load():
             button.SelectableLabel,
             [[(0,0), 'Human',20], [(0,0), 'Bot',20]]
         )
-        player1_type_select.buttons[0].handle_selected()
-        player1_type_select.selected_button = player1_type_select.buttons[0]
-        versusmode.local_state.player_type_dictionary[PlayerPositions.PLAYER2] = PlayerTypes.HUMAN
+        self.player1_type_select.buttons[0].handle_selected()
+        self.player1_type_select.selected_button = self.player1_type_select.buttons[0]
+        #versusmode.local_state.player_type_dictionary[PlayerPositions.PLAYER2] = PlayerTypes.HUMAN
         
-        player1_difficulty_select_position = (50, player1_type_select.height + 10)
-        player1_difficulty_select = wotsuicontainers.ButtonContainer(
+        player1_difficulty_select_position = (50, self.player1_type_select.height + 10)
+        self.player1_difficulty_select = wotsuicontainers.ButtonContainer(
             player1_difficulty_select_position,
             100,
             350,
@@ -80,29 +60,29 @@ def load():
                 ['Challenge', Difficulties.CHALLENGE]
             ]
         )
-        player1_difficulty_select.inactivate()
+        self.player1_difficulty_select.inactivate()
         
         player1_moveset_select_position = (
             50, 
-            player1_difficulty_select.position[1] + player1_type_select.height
+            self.player1_difficulty_select.position[1] + self.player1_type_select.height
         )
-        player1_moveset_select = MovesetSelector(
+        self.player1_moveset_select = MovesetSelector(
             player1_moveset_select_position,
             playable_movesets
         )
         player1_widget_position = (
             50, 
-            player1_moveset_select.position[1] + player1_moveset_select.height + 10
+            self.player1_moveset_select.position[1] + self.player1_moveset_select.height + 10
         )
-        player1_stats_widget = PlayerStatsWidget(player1_widget_position)
+        self.player1_stats_widget = PlayerStatsWidget(player1_widget_position)
         
         player1_color_select_position = (
-            player1_widget_position[0] + player1_stats_widget.width + 100,
+            player1_widget_position[0] + self.player1_stats_widget.width + 100,
             player1_widget_position[1] + 50
         )
-        player1_color_select = ColorWheel(player1_color_select_position, 0)
+        self.player1_color_select = ColorWheel(player1_color_select_position, 0)
         
-        player2_type_select = wotsuicontainers.ButtonContainer(
+        self.player2_type_select = wotsuicontainers.ButtonContainer(
             (450,50),
             120,
             300,
@@ -110,12 +90,12 @@ def load():
             button.SelectableLabel,
             [[(0,0), 'Human',20], [(0,0), 'Bot',20]]
         )
-        player2_type_select.buttons[1].handle_selected()
-        player2_type_select.selected_button = player2_type_select.buttons[1]
-        versusmode.local_state.player_type_dictionary[PlayerPositions.PLAYER2] = PlayerTypes.BOT
+        self.player2_type_select.buttons[1].handle_selected()
+        self.player2_type_select.selected_button = self.player2_type_select.buttons[1]
+        #versusmode.local_state.player_type_dictionary[PlayerPositions.PLAYER2] = PlayerTypes.BOT
         
-        player2_difficulty_select_position = (450, player2_type_select.height + 10)
-        player2_difficulty_select = wotsuicontainers.ButtonContainer(
+        player2_difficulty_select_position = (450, self.player2_type_select.height + 10)
+        self.player2_difficulty_select = wotsuicontainers.ButtonContainer(
             player2_difficulty_select_position,
             100,
             350,
@@ -128,79 +108,84 @@ def load():
                 ['Challenge', Difficulties.CHALLENGE]
             ]
         )
-        player2_difficulty_select.activate()
-        player2_difficulty_select.buttons[0].handle_selected()
-        player2_difficulty_select.selected_button = player2_difficulty_select.buttons[0]
+        self.player2_difficulty_select.activate()
+        self.player2_difficulty_select.buttons[0].handle_selected()
+        self.player2_difficulty_select.selected_button = self.player2_difficulty_select.buttons[0]
         
         player2_moveset_select_position = (
             450, 
-            player2_difficulty_select.position[1] + player2_type_select.height
+            self.player2_difficulty_select.position[1] + self.player2_type_select.height
         )
-        player2_moveset_select = MovesetSelector(
+        self.player2_moveset_select = MovesetSelector(
             player2_moveset_select_position,
             playable_movesets
         )
         player2_widget_position = (
             450, 
-            player2_moveset_select.position[1] + player2_moveset_select.height + 10
+            self.player2_moveset_select.position[1] + self.player2_moveset_select.height + 10
         )
-        player2_stats_widget = PlayerStatsWidget(player2_widget_position)
+        self.player2_stats_widget = PlayerStatsWidget(player2_widget_position)
         
         player2_color_select_position = (
-            player2_widget_position[0] + player2_stats_widget.width + 100,
+            player2_widget_position[0] + self.player2_stats_widget.width + 100,
             player2_widget_position[1] + 50
         )
-        player2_color_select = ColorWheel(player2_color_select_position, 3)
+        self.player2_color_select = ColorWheel(player2_color_select_position, 3)
+        
+loaded = False
+exit_button = None
+UI_objects = None
+
+def get_playable_movesets():
+    movesets = movesetdata.get_movesets()
+    playable_movesets = [moveset for moveset in movesets if moveset.is_complete()]
+    
+    return playable_movesets
+
+def load():
+    global loaded
+    global exit_button
+    global UI_objects
+    
+    exit_button = button.ExitButton()
+    loaded = True
+    
+    if UI_objects == None:
+        UI_objects = VersusMovesetSelectUiObjects()
+    
+    UI_objects.start_match_label.handle_deselected()
     
 def unload():
     global loaded
     global exit_button
-    global start_match_label
     
     exit_button = None
     loaded = False
-    start_match_label = None
 
 def clear_data():
-    global player1_type_select
-    global player1_difficulty_select
-    global player1_moveset_select
-    global player1_stats_widget
-    global player1_color_select
-    global player2_type_select
-    global player2_difficulty_select
-    global player2_moveset_select
-    global player2_stats_widget
-    global player2_color_select
+    global UI_objects
     
-    player1_type_select = None
-    player1_difficulty_select = None
-    player1_moveset_select = None
-    player1_stats_widget = None
-    player1_color_select = None
-    player2_type_select = None
-    player2_difficulty_select = None
-    player2_moveset_select = None
-    player2_stats_widget = None
-    player2_color_select = None
+    UI_objects = None
 
 def handle_events():
     global loaded
     global exit_button
-    global start_match_label
-    global player1_type_select
-    global player1_difficulty_select
-    global player1_moveset_select
-    global player1_stats_widget
-    global player1_color_select
-    global player2_type_select
-    global player2_difficulty_select
-    global player2_moveset_select
-    global player2_stats_widget
-    global player2_color_select
+    global UI_Objects
     
     if loaded == False:
         load()
+    
+    start_match_label = UI_objects.start_match_label
+    player1_type_select = UI_objects.player1_type_select 
+    player1_difficulty_select = UI_objects.player1_difficulty_select
+    player1_moveset_select = UI_objects.player1_moveset_select
+    player1_stats_widget = UI_objects.player1_stats_widget
+    player1_color_select = UI_objects.player1_color_select
+    player2_type_select = UI_objects.player2_type_select
+    player2_difficulty_select = UI_objects.player2_difficulty_select
+    player2_moveset_select = UI_objects.player2_moveset_select
+    player2_stats_widget = UI_objects.player2_stats_widget
+    player2_color_select = UI_objects.player2_color_select
     
     if pygame.MOUSEBUTTONDOWN in wotsuievents.event_types:
         if exit_button.contains(wotsuievents.mouse_pos):
